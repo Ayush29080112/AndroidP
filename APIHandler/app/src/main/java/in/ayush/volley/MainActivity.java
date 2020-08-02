82513046e69e30ffea96f45a7b93ca6a6e827c7d
Ayush Bajpai <ayushbajpai1401@gmail.com>
2020-08-02 16:01:55 +0530

    Adding a sample Json to use it as API

--- APIHandler/app/src/main/java/in/ayush/volley/MainActivity.java
+++ APIHandler/app/src/main/java/in/ayush/volley/MainActivity.java
@@ -3,12 +3,60 @@ package in.ayush.volley;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView textView;
+    private Button button;
+    private RequestQueue requestQueue;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.textView);
+        button = findViewById(R.id.button);
+
+        requestQueue = Volley.newRequestQueue(this);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sendApiRequest();
+            }
+        });
+
+    }
+
+    private void sendApiRequest() {
+        String url = "https://github.com/Ayush29080112/AndroidP/blob/master/VolleyOne.json";
+
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+              Log.e("Json array", response);
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e("Error", error.getLocalizedMessage());
+            }
+        });
+
+        requestQueue.add(request);
     }
 }
