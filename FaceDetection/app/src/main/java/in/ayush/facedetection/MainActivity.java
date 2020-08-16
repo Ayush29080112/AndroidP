976e5b2f8c208a1e15f332ef068204306e551563
Ayush Bajpai <ayushbajpai1401@gmail.com>
2020-08-16 14:13:07 +0530

    added face detection app

---
+++ FaceDetection/app/src/main/java/in/ayush/facedetection/MainActivity.java
@@ -0,0 +1,101 @@
+package in.ayush.facedetection;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.ml.vision.FirebaseVision;
+import com.google.firebase.ml.vision.common.FirebaseVisionImage;
+import com.google.firebase.ml.vision.face.FirebaseVisionFace;
+import com.google.firebase.ml.vision.face.FirebaseVisionFaceDetector;
+import com.google.firebase.ml.vision.face.FirebaseVisionFaceDetectorOptions;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button cameraButton;
+    private final static int REQUEST_IMAGE_CAPTURE = 123;
+    private FirebaseVisionImage image;
+    private FirebaseVisionFaceDetector detector;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        FirebaseApp.initializeApp(this);
+        cameraButton = findViewById(R.id.camera_button);
+        cameraButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                if(takePictureIntent.resolveActivity(getPackageManager()) !=null){
+                    startActivityForResult(takePictureIntent,REQUEST_IMAGE_CAPTURE);
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap datas = (Bitmap) extras.get("data");
+            detectFace(datas);
+        }
+    }
+
+    private void detectFace(Bitmap datas) {
+
+        // High-accuracy landmark detection and face classification
+        FirebaseVisionFaceDetectorOptions highAccuracyOpts =
+                new FirebaseVisionFaceDetectorOptions.Builder()
+                        .setModeType(FirebaseVisionFaceDetectorOptions.ACCURATE_MODE)
+                        .setClassificationType(FirebaseVisionFaceDetectorOptions.ALL_LANDMARKS)
+                        .setClassificationType(FirebaseVisionFaceDetectorOptions.ALL_CLASSIFICATIONS)
+                        .setMinFaceSize(0.15f)
+                        .setTrackingEnabled(true)
+                        .build();
+        try {
+            image = FirebaseVisionImage.fromBitmap(datas);
+            detector = FirebaseVision.getInstance().getVisionFaceDetector(highAccuracyOpts);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        detector.detectInImage(image).addOnSuccessListener(new OnSuccessListener<List<FirebaseVisionFace>>() {
+            @Override
+            public void onSuccess(List<FirebaseVisionFace> firebaseVisionFaces) {
+                String resultText = "";
+                int i = 1;
+                if(firebaseVisionFaces.size() == 0){
+                    Toast.makeText(MainActivity.this,"No Faces Found!!!",Toast.LENGTH_SHORT).show();
+                }else {
+                    for (FirebaseVisionFace face : firebaseVisionFaces) {
+                        resultText = resultText.concat("\n" + i + ".")
+                                .concat("\n Smile" + face.getSmilingProbability() * 100 + "%")
+                                .concat("\n Right Eye Open " + face.getRightEyeOpenProbability() * 100 + "%")
+                                .concat("\n Left Eye Open " + face.getLeftEyeOpenProbability() * 100 + "%");
+                        i++;
+                    }
+
+                    Bundle bundle = new Bundle();
+                    bundle.putString(FaceDetection.RESULT_TEXT,resultText);
+                    DialogFragment resultDialog = new ResultDialog();
+                    resultDialog.setArguments(bundle);
+                    resultDialog.setCancelable(true);
+                    resultDialog.show(getSupportFragmentManager(),FaceDetection.RESULT_DIALOG);
+                }
+            }
+        });
+    }
+}
---
+++ FireFileUpload/app/src/main/java/in/ayush/firefileupload/MainActivity.java
@@ -0,0 +1,74 @@
+package in.ayush.firefileupload;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final int GALLERY = 4;
+    StorageReference storageReference;
+    Button upload;
+    ImageView image;
+
+    ProgressDialog progressDialog;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        upload = findViewById(R.id.upload);
+        image = findViewById(R.id.image);
+        storageReference = FirebaseStorage.getInstance().getReference();
+        progressDialog = new ProgressDialog(this);
+        upload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent.setType("image/*");
+                startActivityForResult(intent,GALLERY);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == GALLERY){
+            progressDialog.setMessage("Uploading");
+            Uri uri = data.getData();
+            image.setImageURI(uri);
+            progressDialog.show();
+            StorageReference photo_name = storageReference.child("Photos/"+uri.getLastPathSegment()+".png");
+            photo_name.putFile(uri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    progressDialog.dismiss();
+                    Toast.makeText(MainActivity.this,"Uploaded Successfully",Toast.LENGTH_SHORT).show();
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    progressDialog.dismiss();
+                    Toast.makeText(MainActivity.this,"Uploaded Failed!!!",Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+    }
+}
