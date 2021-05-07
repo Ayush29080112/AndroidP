ccf4f976d07ca421e50662e2a5dafe8f6a91842c
System Administrator <ayushbajpai1401@gmail.com>
2021-05-07 20:33:19 +0530

    Apk added

--- MyApplication/app/src/main/java/in/ayush/audioplayer/MainActivity.java
+++ MyApplication/app/src/main/java/in/ayush/audioplayer/MainActivity.java
@@ -2,22 +2,46 @@ package in.ayush.audioplayer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-    protected void onButtonClicked(View v){
+        button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
 
-        startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.youtube.com/watch?v=Hxy8BZGQ5Jo")));
-        Log.i("Video", "Video Playing....");
+                Intent myIntent= new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.youtube.com/watch?v=Hxy8BZGQ5Jo"));
+                PendingIntent pendingIntent
+                        = PendingIntent.getBroadcast(getBaseContext(),
+                        0, myIntent, 0);
+
+                AlarmManager alarmManager
+                        = (AlarmManager)getSystemService(ALARM_SERVICE);
+                Calendar calendar = Calendar.getInstance();
+                calendar.setTimeInMillis(System.currentTimeMillis());
+                calendar.add(Calendar.SECOND, 10);
+                long interval = 60 * 1000; //
+                alarmManager.setRepeating(AlarmManager.RTC_WAKEUP,
+                        calendar.getTimeInMillis(), interval, pendingIntent);
+                finish();
+            }});
+
+
     }
+
 }
