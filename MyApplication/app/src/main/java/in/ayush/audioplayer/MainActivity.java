5abc01683286cf8673d48bb44fba295f44bc20c5
System Administrator <ayushbajpai1401@gmail.com>
2021-05-07 19:51:10 +0530

    Apk added

---
+++ MyApplication/app/src/main/java/in/ayush/audioplayer/MainActivity.java
@@ -0,0 +1,23 @@
+package in.ayush.audioplayer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    protected void onButtonClicked(View v){
+
+        startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.youtube.com/watch?v=Hxy8BZGQ5Jo")));
+        Log.i("Video", "Video Playing....");
+    }
+}
